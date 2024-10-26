69d0df6e30603a12a8656eb31bb990ecc9e96838
Vitor <vitorhenrique_rgt@outlook.com>
2024-10-26 11:41:19 -0300

    update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,15 @@
-
 const dotenv = require('dotenv');
 dotenv.config();
+
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectModule: require('pg'),
+    dialectOptions: { ssl: { require: true } },
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectModule: require('pg'),
     dialectOptions: { ssl: { require: true } },
