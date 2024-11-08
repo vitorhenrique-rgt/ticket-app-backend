4ae8334b96ea2daf06d813b543c3526b6168a646
Vitor <vitorhenrique_rgt@outlook.com>
2024-11-08 18:55:58 -0300

    update cors

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const sequelize = require('./config/database');
-const companyRoutes = require('./routes/companyRoutes');
-const userRoutes = require('./routes/userRoutes');
-const ticketRoutes = require('./routes/ticketRoutes')
-const tagRoutes = require('./routes/tagRoutes');
+const express = require("express");
+const cors = require("cors");
+const sequelize = require("./config/database");
+const companyRoutes = require("./routes/companyRoutes");
+const userRoutes = require("./routes/userRoutes");
+const ticketRoutes = require("./routes/ticketRoutes");
+const tagRoutes = require("./routes/tagRoutes");
 const app = express();
-app.use(cors());
+app.use(cors(process.env.CORS));
 app.use(express.json());
 
-app.use('/api', companyRoutes);
-app.use('/api', userRoutes);
-app.use('/api', ticketRoutes);
-app.use('/api', tagRoutes);
+app.use("/api", companyRoutes);
+app.use("/api", userRoutes);
+app.use("/api", ticketRoutes);
+app.use("/api", tagRoutes);
 
 const PORT = process.env.PORT || 3000;
 
@@ -21,6 +21,6 @@ app.listen(PORT, async () => {
     await sequelize.authenticate();
     console.log(`Servidor rodando na porta ${PORT}`);
   } catch (error) {
-    console.error('Erro ao conectar ao banco:', error);
+    console.error("Erro ao conectar ao banco:", error);
   }
 });
