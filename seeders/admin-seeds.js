ea13857f87e1232be4e2330efc58c56e4b92a940
Vitor <vitorhenrique_rgt@outlook.com>
2024-11-08 15:01:31 -0300

    remove vercel file

--- seeders/admin-seeds.js
+++ seeders/admin-seeds.js
@@ -1,4 +1,5 @@
 const { User, Company } = require("../models");
+const bcrypt = require("bcrypt"); // Importa o bcrypt
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -13,23 +14,32 @@ module.exports = {
         company = await Company.create({ name: "DefaultCompany" });
       }
 
-      // Cria um usuário admin com o nickname 'ticket'
-      await User.create({
-        name: "Admin",
-        nickname: "admin",
-        password: "admin",
-        isAdmin: true,
-        companyId: company.id,
-      });
+      // Verifica se o usuário admin já existe
+      const adminExists = await User.findOne({ where: { nickname: "admin" } });
 
-      console.log("Usuário admin criado com sucesso!");
+      if (!adminExists) {
+        // Criptografa a senha usando bcrypt
+        const hashedPassword = bcrypt.hashSync("admin", 10); // 10 é o número de salt rounds
+
+        // Cria um usuário admin com a senha criptografada
+        await User.create({
+          name: "Admin",
+          nickname: "admin",
+          password: hashedPassword, // Usa a senha criptografada
+          isAdmin: true,
+          companyId: company.id,
+        });
+        console.log("Usuário admin criado com sucesso!");
+      } else {
+        console.log("Usuário admin já existe.");
+      }
     } catch (error) {
       console.error("Erro ao criar usuário admin:", error);
     }
   },
 
   async down(queryInterface, Sequelize) {
-    // Este método pode ser usado para reverter as seeds, caso necessário
+    // Este método reverte o seed caso necessário
     await User.destroy({ where: { nickname: "admin" } });
     await Company.destroy({ where: { name: "DefaultCompany" } });
   },
